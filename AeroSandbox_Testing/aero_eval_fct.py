fixed CG calculation

AeroSandbox_Testing/aero_eval_fct.py
@@ -104,8 +104,9 @@ def main(span=0.8,
     # -----------------------
     # Create airplane
     # -----------------------
-    x_CG_geom = span/6 * (root_chord + 2*tip_chord) / (root_chord + tip_chord) *np.tan(sweep)
-    x_CG = x_CG_geom * 0.8 # account for battery / electronics being heavier
+    sweep_05 = np.arctan(np.tan(sweep) - 2 / (aspect_ratio) * (1 - taper_ratio) / (1 + taper_ratio))
+    x_CG_geom = span/6 * (root_chord + 2*tip_chord) / (root_chord + tip_chord) *np.tan(sweep_05)
+    x_CG = x_CG_geom * 0.8 + 0.5*root_chord # account for battery / electronics being heavier
     airplane = asb.Airplane(
         wings=[wing, winglet],
         s_ref=s_ref,